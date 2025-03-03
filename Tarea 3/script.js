c1e7fd592bcf5f689cf4078d81d17337f89ab6e5
Heber Vazquez <gerber@Laptop-de-Heber.local>
2025-03-02 21:01:40 -0600

    Tarea 3

---
+++ Tarea 3/script.js
@@ -0,0 +1,13 @@
+let p1 ="El uso de tecnologías web como HTML, CSS o Java Script ha permitido abrir nuevas posibilidades en torno a la creación y expresión artística, ya que ahora una obra no solo se limita a existir bajo cuatro paredes, sino que puede expandirse más allá de un entorno meramente material, trascienden el espacio y tiempo ya que pueden permitirse que se aprecien en varios lugares al mismo tiempo y en distintos tiempo, sin que se limiten al horario y disponibilidad de un salón. Creo que esa es la gran virtud del arte web, una característica que me permite expandir mi trabajo para exhibirlo.";
+let p2 = "Por otro lado, creo que la web me permite aprovechar su formato interactivo para intentar conectar mejor con cualquier persona sin necesidad de que sientan que es aburrida una obra porque solo la están viendo, incluso creo que esa interactividad que puedo darle a las obras ayudan a despertar la curiosidad de los usuarios, con lo que ellos se vuelven una parte importante para la obra, haciendo a su vez que puedan entender mejor lo que quiero decir con cada pieza. Además claro que me permiten aprovechar habilidades que tengo en torno a lo digital como la creación audiovisual, sin necesidad de que salga a flote mi poca habilidad plástica y manual como con el arte clásico. En fin, estas creo que son las principales ventajas que yo veo y aprovecho para mi realización y expresión como artista en formación.";
+
+
+document.getElementById('parrafo-uno').innerHTML=p1;
+document.getElementById('parrafo-dos').innerHTML=p2;
+
+
+console.log(p1.length);
+console.log(p2.length);
+
+
+//Hacer un contador de palabras para cada párrafo
